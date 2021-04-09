d14d1f230b91dc003151c545c8d7759b7a831d47
ColourfulBlack <kalicock@outlook.com>
2021-04-09 14:47:05 +0800

    test reco theme function

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,24 @@ module.exports = {
     title: "ColourfulBlack's Blog",
     description: "Just For Fun.",
     base: "/",
-    theme: "vuepress-theme-reco"
+    theme: "vuepress-theme-reco",
+    themeConfig: {
+        type: "blog",
+        nav: [
+            { text: "时间轴", link: "/timeline/", icon: "reco-date" }
+        ],
+        blogConfig: {
+            category: {
+                location: 1,
+                text: '分类'
+            },
+            tag: {
+                localtion: 2,
+                text: '标签'
+            },
+            socialLinks: [
+                { icon: "reco-github", link: "https://github.com/ColourfulBlack" }
+            ]
+        }
+    }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,14 @@
+---
+title: Hello World!
+lang: zh-CN
+date: 2021-04-09
+sidebar: 'auto'
+categories:
+  - test
+tags:
+  - test
+---
+
 # HELLO WORLD
 
 Very First Page
